7daa97165bc8c3409761aa11247e0500b4428a43
Marco Lanaro <marcolanaro@gmail.com>
2018-10-23 17:51:43 +0100

    Inline render svg at build time

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
  
 module.exports = {
   entry: {
@@ -15,7 +16,10 @@ module.exports = {
     new WebpackCleanupPlugin(),
     new CopyWebpackPlugin([{ from: 'public', to: '.' }]),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
+    }),
+    new HtmlWebpackInlineSVGPlugin({
+      runPreEmit: true,
     })
   ]
 };
