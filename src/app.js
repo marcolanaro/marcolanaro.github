6abfa871f193e5b882ddc206186016c95882804d
Marco Lanaro <marcolanaro@gmail.com>
2018-11-07 15:26:06 +0000

    Add prettier config

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-import "./styles.css";
+import './styles.css';
 
 // CSS Selectors
-const CONTENT = "content";
-const BACK_BUTTON = "back-button";
-const BACK_BUTTON_HIDDEN = "back-button-hidden";
-const PANEL_HIDE = "panel-hide";
-const PANEL_HIDDEN = "panel-hidden";
-const CIRCLE = "circle";
-const CIRCLE_TRANSPARENT = "circle-transparent";
-const ANIMATE_CIRCLE_TRASPARENCY = "circle-animation-trasparency";
-const ANIMATE_CIRCLE_EXPAND = "circle-animate-expand";
+const CONTENT = 'content';
+const BACK_BUTTON = 'back-button';
+const BACK_BUTTON_HIDDEN = 'back-button-hidden';
+const PANEL_HIDE = 'panel-hide';
+const PANEL_HIDDEN = 'panel-hidden';
+const CIRCLE = 'circle';
+const CIRCLE_TRANSPARENT = 'circle-transparent';
+const ANIMATE_CIRCLE_TRASPARENCY = 'circle-animation-trasparency';
+const ANIMATE_CIRCLE_EXPAND = 'circle-animate-expand';
 
 const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
 
@@ -22,7 +22,7 @@ const getFirstElementByClassName = classname =>
     ? getElementsByClassName(classname)[0]
     : undefined;
 const getElementsById = id => document.getElementById(id);
-const content = getFirstElementByClassName("content");
+const content = getFirstElementByClassName('content');
 const appendChildToContent = child => content.appendChild(child);
 const getEntries = collection =>
   Object.keys(collection).map(id => collection[id]);
@@ -71,7 +71,7 @@ const removeEventListener = compose(
   _removeEventListener
 );
 const removeTransitionEnd = callback => el =>
-  removeEventListener({ event: "transitionend", callback })(el);
+  removeEventListener({ event: 'transitionend', callback })(el);
 const parent = (...fns) => el => {
   if (el && el.parentElement) {
     compose(...fns)(el.parentElement);
@@ -88,12 +88,12 @@ const rafFlipClass = className =>
     raf(removeClass(className)),
     addClass(className)
   );
-const createA = () => createElement("a");
+const createA = () => createElement('a');
 const removeDOM = element => (!!element ? element.remove() : null);
 
 // State
 const state = {
-  pageName: undefined
+  pageName: undefined,
 };
 
 // Panels
@@ -106,7 +106,7 @@ const handlePanelHidden = compose(
   handleTarget
 );
 const hidePanel = compose(
-  addEventListener({ event: "transitionend", callback: handlePanelHidden }),
+  addEventListener({ event: 'transitionend', callback: handlePanelHidden }),
   addClass(PANEL_HIDE),
   getPage
 );
@@ -152,7 +152,7 @@ const showBackButton = compose(
   getBackButton
 );
 const createBackButton = compose(
-  addEventListener({ event: "click", callback: close }),
+  addEventListener({ event: 'click', callback: close }),
   appendChildToContent,
   innerHTML(`
     <svg fill="#FFFFFF" viewBox="0 0 24 24">
@@ -162,8 +162,8 @@ const createBackButton = compose(
   `),
   hideBackButtonSelector,
   addClass(BACK_BUTTON),
-  setAttribute({ name: "link", value: "#" }),
-  setAttribute({ name: "title", value: "Back" }),
+  setAttribute({ name: 'link', value: '#' }),
+  setAttribute({ name: 'title', value: 'Back' }),
   createA
 );
 
@@ -177,13 +177,13 @@ const circles = compose(
   getEntries,
   getElementsByClassName
 )(CIRCLE);
-circles.forEach(addEventListener({ event: "click", callback: open }));
+circles.forEach(addEventListener({ event: 'click', callback: open }));
 const setParentParentDisplay = value => el =>
   (el.parentElement.parentElement.style.display = value);
 const removeHandlerSiblingsHide = el =>
   removeTransitionEnd(handleSiblingsHide)(el);
 const handleSiblingsHide = compose(
-  setParentParentDisplay("none"),
+  setParentParentDisplay('none'),
   removeHandlerSiblingsHide,
   handleTarget
 );
@@ -192,8 +192,8 @@ const hideCircleSiblings = pageName =>
     compose(
       rafFlipClass(ANIMATE_CIRCLE_TRASPARENCY),
       addEventListener({
-        event: "transitionend",
-        callback: handleSiblingsHide
+        event: 'transitionend',
+        callback: handleSiblingsHide,
       }),
       addClass(CIRCLE_TRANSPARENT)
     )
@@ -205,7 +205,7 @@ const getSiblingsCircle = compose(
 const showSiblings = pageName =>
   getSiblingsCircle(pageName).forEach(
     compose(
-      setParentParentDisplay(""),
+      setParentParentDisplay(''),
       removeClass(CIRCLE_TRANSPARENT)
     )
   );
@@ -217,14 +217,14 @@ const getCircleScale = el => {
   return scale;
 };
 const getAnimationOptions = status => el => {
-  setTransform("")(el);
+  setTransform('')(el);
   const circleScale = getCircleScale(el);
-  const isOpening = status === "open";
+  const isOpening = status === 'open';
   const parent = el.parentElement;
   const first = parent.getBoundingClientRect();
 
   const transform = isOpening ? addClass : removeClass;
-  transform("circleContainer-expanded")(parent);
+  transform('circleContainer-expanded')(parent);
   const last = parent.getBoundingClientRect();
 
   const scaleX = first.width / last.width;
@@ -245,13 +245,13 @@ const getAnimationOptions = status => el => {
       child: {
         before: {
           scaleX: isOpening ? 1 / scaleX : (1 / scaleX) * circleScale,
-          scaleY: isOpening ? 1 / scaleY : (1 / scaleY) * circleScale
+          scaleY: isOpening ? 1 / scaleY : (1 / scaleY) * circleScale,
         },
         after: {
-          scale: isOpening ? getCircleScale(el) : 1
-        }
-      }
-    }
+          scale: isOpening ? getCircleScale(el) : 1,
+        },
+      },
+    },
   };
 };
 const removeHandlerEndOpenCircle = el =>
@@ -284,9 +284,9 @@ const animateCircleToHeader = ({ options, scaleCircle, listener }) =>
     raf(
       setTransform(`scale(${options.child.after.scale})`),
       addClass(ANIMATE_CIRCLE_EXPAND),
-      parent(setTransform(""), addClass(ANIMATE_CIRCLE_EXPAND))
+      parent(setTransform(''), addClass(ANIMATE_CIRCLE_EXPAND))
     ),
-    addEventListener({ event: "transitionend", callback: listener }),
+    addEventListener({ event: 'transitionend', callback: listener }),
     setTransform(
       `scale(${options.child.before.scaleX}, ${options.child.before.scaleY})`
     ),
@@ -298,5 +298,5 @@ const setCircleHeader = (status, listener) =>
     getAnimationOptions(status),
     getCircle
   );
-const addCircleToHeader = setCircleHeader("open", handleEndOpenCircle);
-const removeCircleToHeader = setCircleHeader("close", handleEndCloseCircle);
+const addCircleToHeader = setCircleHeader('open', handleEndOpenCircle);
+const removeCircleToHeader = setCircleHeader('close', handleEndCloseCircle);
